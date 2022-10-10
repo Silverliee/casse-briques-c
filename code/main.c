aea06e40b015f778945161ca2178f7dac502c37a
BGquent753 <106093587+BGquent753@users.noreply.github.com>
2022-10-10 11:05:54 +0200

    Add files via upload

--- code/main.c
+++ code/main.c
@@ -2,28 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-char map[5][9] = {
-    {'x','x','x','x','x','x','x','x','x'},
-    {'x','p',' ','m','m','m','m','m','x'},
-    {'x',' ','x','m','x','m','x',' ','x'},
-    {'x','m','m','m','m','m',' ','p','x'},
-    {'x','x','x','x','x','x','x','x','x'}
+int map[5][9] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 4, 2, 1, 1, 1, 1, 1, 0},
+    {0, 2, 0, 1, 0, 1, 0, 2, 0},
+    {0, 1, 1 ,1, 1, 1, 2, 5, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0}
 };
 
 int main(){
+    int nbJoueurs = 1;
     for(int i = 0;i < 5; i++){
         for(int j = 0; j < 9; j++){
-            if(map[i][j] == 'x'){
+            if(map[i][j] == 0){
                 printf("%c", 219);
             }
-            else if(map[i][j] == 'm'){
+            else if(map[i][j] == 1){
                 printf("%c", 177);
             }
-            else if(map[i][j] == ' '){
-                printf("%c", map[i][j]);
+            else if(map[i][j] == 2){
+                printf("%c", ' ');
             }
             else{
-                printf("%c", 'J');
+                printf("%d", map[i][j]-3);
             }
         }
         printf("\n");
